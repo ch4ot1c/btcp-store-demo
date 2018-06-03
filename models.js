06bec867eb29753c515649f7a791285f92a79980
Jon Layton <me@jonl.io>
2018-06-03 17:23:17 +0000

    Need Tip - another ws for store-demo is unnecessary and detached from the daemon; logic should be merged into  service in bitcore-node-btcp for least overhead

--- models.js
+++ models.js
@@ -6,31 +6,49 @@ var ObjectId = mongoose.Schema.Types.ObjectId;
 
 var MerchantSchema = new Schema({
   xpub: {type: String, required: true},
-  address_index: {type: Number, default: 0} 
+  next_address_index: {type: Number, default: 0} 
 }, {timestamps: true});
 
 var Merchant = mongoose.model('Merchant', MerchantSchema);
+
+// --- BlockManager ---
+// If a restart is required, we rescan from last_block_height to ensure no txs are missed
+
+var BlockManagerSchema = new Schema({
+  last_block_height: {type: String, required: true, default: 0},
+}, {timestamps: true});
+
+var BlockManager = mongoose.model('BlockManager', BlockManagerSchema);
 
 // --- Product ---
 
 var ProductSchema = new Schema({
   price_satoshis: {type: Number, required: true},
-  name: String 
+  address_btcp: {type: String, required: true},
+  address_index: {type: String, required: false},
+  name: {type: String, required: true}
 }, {timestamps: true});
 
 var Product = mongoose.model('Product', ProductSchema);
 
-// --- Invoice ---
+// --- Transaction - Added once first seen (if relevant) ---
 
-var InvoiceSchema = new Schema({
-  product_id: {type: ObjectId, ref: 'Product', required: true},
-  total_satoshis: {type: Number, required: true},
-  address_index: {type: Number, required: true},
+var TransactionSchema = new Schema({
+  product_id: {type: ObjectId, ref: 'Product', index: true, required: true},
+  receiving_address: {type: String, required: true},
   user_address: String,
-  blockchain_tx_id: String
+  satoshis: {type: Number, required: true},
+  blockchain_tx_id: String,
+  // Eventually...
+  block_mined: Number
 }, {timestamps: true});
 
-var Invoice = mongoose.model('Invoice', InvoiceSchema);
+var Transaction = mongoose.model('Transaction', TransactionSchema);
 
 
-module.exports = {Merchant: Merchant, Product: Product, Invoice: Invoice};
+module.exports = {
+  Merchant: Merchant,
+  Product: Product,
+  Transaction: Transaction,
+  BlockManager: BlockManager
+};
